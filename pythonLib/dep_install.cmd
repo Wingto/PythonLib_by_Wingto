5f094e740c7e53d10d9b9d056f2597c4d499b43d
Wingto <50476460+Wingto@users.noreply.github.com>
2022-09-07 17:46:44 +0800

    updated

--- pythonLib/dep_install.cmd
+++
@@ -1,6 +0,0 @@
-
-python -m pip install --user --upgrade pip 
-pip3 install -r "E:/Git_2022/Plugin/PythonLib_by_Wingto/pythonLib/requirements.txt" --target="E:/Git_2022/Plugin/PythonLib_by_Wingto/pythonLib/dep/python" -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
-
-
-
